1b9c5ceac5a4d97e6bdcb0d62020bca8b040bae3
Nathan Cantuares dos Santos <nathaan30@gmail.com>
2026-04-01 21:14:28 -0300

    Finalização da Atividade

--- praticas/pratica01/atv-pratica01/screens/DespesasRecentes.js
+++ praticas/pratica01/atv-pratica01/screens/DespesasRecentes.js
@@ -1,11 +1,37 @@
-import {Text} from 'react-native'
+import DespesaItem from "../components/despesa/DespesaItem";
+import DespesaSaida from "../components/despesa/DespesaSaida";
 
-function DespesaRecente(){
+function DespesaRecente() {
+  function filtrarUltimos70Dias(despesas) {
+    const hoje = new Date();
+    const seteDiasAtras = new Date();
+    seteDiasAtras.setDate(hoje.getDate() - 7);
 
-    return (
-        <Text>Despesa Recente</Text>
-    )
+    return despesas.filter((despesa) => {
+      return despesa.data >= seteDiasAtras && despesa.data <= hoje;
+    });
+  }
 
+  const DUMMY_EMPESAS = [
+    {
+      id: "1",
+      descricao: "Conta de Luz",
+      valor: 100.99,
+      data: new Date(2026, 2, 26),
+    },
+    {
+      id: "2",
+      descricao: "Conta de Agua",
+      valor: 40.99,
+      data: new Date(2026, 2, 31),
+    },
+  ];
+
+  return (
+    <DespesaSaida
+      despesas={filtrarUltimos70Dias(DUMMY_EMPESAS)}
+      periodo={"Últimos 7 dias"}
+    />
+  );
 }
-
-export default DespesaRecente
+export default DespesaRecente;
--- praticas/pratica01/atv-pratica01/screens/TodasDespesas.js
+++ praticas/pratica01/atv-pratica01/screens/TodasDespesas.js
@@ -1,26 +1,22 @@
-import DespesaSaida from "../components/despesa/DespesaSaida"
+import DespesaSaida from "../components/despesa/DespesaSaida";
 
-function TodasDespesas(){
+function TodasDespesas() {
+  const DUMMY_EMPESAS = [
+    {
+      id: "1",
+      descricao: "Conta de Luz",
+      valor: 100.99,
+      data: new Date(2025, 2, 11),
+    },
+    {
+      id: "2",
+      descricao: "Conta de Agua",
+      valor: 40.99,
+      data: new Date(2025, 4, 10),
+    },
+  ];
 
-    const DUMMY_EMPESAS = [
-        {
-            id: '1',
-            descricao: 'Conta de Luz',
-            valor: 100.99,
-            data: new Date(2025, 2, 11)
-        },  
-        {
-            id: '2',
-            descricao: 'Conta de Agua',
-            valor: 40.99,
-            data: new Date(2025, 4, 10)
-        }
-    ]
-
-    return (
-        <DespesaSaida despesas={DUMMY_EMPESAS} periodo={'Total'} />
-    )
-
+  return <DespesaSaida despesas={DUMMY_EMPESAS} periodo={"Total"} />;
 }
 
-export default TodasDespesas
+export default TodasDespesas;
